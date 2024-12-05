517c4d2f9c430236029fcd0dbb91e2318c312868
Tom Ecrepont <Tom Ecrepont>
2024-12-05 16:12:00 +0000

    use the midleware

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -7,13 +7,22 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/troptropcontent/factoche/internal/config"
-	auth_handler "github.com/troptropcontent/factoche/internal/delivery/http/auth"
+
+	auth_handler "github.com/troptropcontent/factoche/internal/delivery/http/handlers/auth"
+	auth_midlewares "github.com/troptropcontent/factoche/internal/delivery/http/midlewares/auth"
 	"github.com/troptropcontent/factoche/internal/infrastructure/database/postgres"
 	auth_repositories "github.com/troptropcontent/factoche/internal/infrastructure/database/repositories/auth"
 	auth_usecase "github.com/troptropcontent/factoche/internal/usecase/auth"
 	"github.com/troptropcontent/factoche/pkg/jwt"
 	"github.com/troptropcontent/factoche/pkg/passhash"
 )
+
+// Here we define the public routes that are not protected by JWT
+// We use the format "METHOD:PATH" to identify the route
+// As the midleware is set globally, we need to define all the public routes here
+var publicRoutes = []string{
+	"POST:/auth/login",
+}
 
 func main() {
 	config := config.NewConfig()
@@ -38,11 +47,18 @@ func main() {
 	// Initialize handlers
 	loginHandler := auth_handler.NewLoginHandler(loginUseCase)
 
+	// Initialize jwt midleware
+	jwtMidleware := auth_midlewares.JWTAuth(auth_midlewares.JWTConfig{
+		JWTService:   jwtService,
+		PublicRoutes: publicRoutes,
+	})
+
 	// Setup server
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
+	e.Use(jwtMidleware)
 
 	// Routes
 	auth := e.Group("auth")
