98387bd2d81f84029a871bba5fc05ad1b27074c7
Tom <tomecrepont@gmail.com>
2024-12-14 12:54:08 +0000

    setup rswag

---
+++ api/config/initializers/rswag.rb
@@ -0,0 +1,7 @@
+# Rswag::Api.configure do |c|
+#   c.swagger_endpoint "/api-docs/v1/swagger.yaml", "API V1 Docs"
+# end
+
+# Rswag::Ui.configure do |c|
+#   c.swagger_endpoint "/api-docs/v1/swagger.yaml", "API V1 Docs"
+# end
