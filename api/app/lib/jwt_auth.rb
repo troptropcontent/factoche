d0e1ceb3548f77b1d79269232278341606cdaedc
Tom <tomecrepont@gmail.com>
2024-12-14 12:53:40 +0000

    add comments

--- api/app/lib/jwt_auth.rb
+++ api/app/lib/jwt_auth.rb
@@ -1,6 +1,29 @@
 module JwtAuth
   ACCESS_TOKEN_EXPIRATION_TIME = 24.hours
   REFRESH_TOKEN_EXPIRATION_TIME = 30.days
+
+  # This method is used to decode the access token
+  # It returns the payload of the token wich is a hash with the following keys:
+  # - sub: the subject of the token (the user id)
+  # - iat: the issued at time
+  # - exp: the expiration time
+  # - jti: the jwt id
+  def self.decode_access_token(token)
+    JWT.decode(token, Rails.application.credentials.token_secrets.access)[0]
+  end
+
+  # This method is used to decode the refresh token
+  # It returns the payload of the token wich is a hash with the following keys:
+  # - sub: the subject of the token (the user id)
+  # - iat: the issued at time
+  # - exp: the expiration time
+  # - jti: the jwt id
+  def self.decode_refresh_token(token)
+    JWT.decode(token, Rails.application.credentials.token_secrets.refresh)[0]
+  end
+
+  # This method is used to generate the access token
+  # It returns the token as a string
   def self.generate_access_token(user_id)
     payload = {
       sub: user_id.to_s,
@@ -14,6 +37,8 @@ module JwtAuth
     )
   end
 
+  # This method is used to generate the refresh token
+  # It returns the token as a string
   def self.generate_refresh_token(user_id)
     payload = {
       sub: user_id.to_s,
