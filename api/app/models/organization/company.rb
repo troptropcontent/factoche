74d9bd7df743ed8fd176ab0590fd2038cf9a3bc3
Tom <tomecrepont@gmail.com>
2024-12-14 16:47:00 +0000

    add relevant associations

--- api/app/models/organization/company.rb
+++ api/app/models/organization/company.rb
@@ -1,4 +1,10 @@
 class Organization::Company < ApplicationRecord
+  has_many :members, dependent: :destroy, class_name: "Organization::Member"
+  has_many :users,
+           through: :members,
+           class_name: "User"
+
+
   validates :name, presence: true
   validates :phone, phone: true, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :members, dependent: :destroy, class_name: "Organization::Member"
+  has_many :companies,
+           through: :members,
+           class_name: "Organization::Company"
 end
