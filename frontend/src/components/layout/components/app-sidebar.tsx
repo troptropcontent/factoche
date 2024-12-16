d9c807b80bebf09a74f35fdd83c571014eb19f92
Tom <tomecrepont@gmail.com>
2024-12-16 14:54:36 +0000

    create sidebar with future sections

--- frontend/src/components/layout/components/app-sidebar.tsx
+++ frontend/src/components/layout/components/app-sidebar.tsx
@@ -1,9 +1,60 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Calendar, Cuboid, Handshake, Home, Inbox, ReceiptText, Search, Settings } from "lucide-react";
 
 export function AppSidebar() {
+  const items = [
+    {
+      title: "Projets",
+      url: "#",
+      icon: Cuboid,
+    },
+    {
+      title: "Factures",
+      url: "#",
+      icon: ReceiptText,
+    },
+    {
+      title: "Clients",
+      url: "#",
+      icon: Handshake,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ];
+
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
-  )
+  );
 }
