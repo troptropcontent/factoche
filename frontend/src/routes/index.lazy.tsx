baf2163caa855d69850e16503e5dfe459366a365
Tom <tomecrepont@gmail.com>
2024-12-16 13:24:46 +0000

    setup chacn

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { useAuth } from '../hooks/use_auth'
+import { Button } from '@/components/ui/button'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -8,8 +9,10 @@ export const Route = createLazyFileRoute('/')({
 function Index() {
   const {logout} = useAuth()
   return (
-    <div ><h1 className='text-sky-700'>Hello from Home!</h1><button onClick={() => {
+    <div ><h1 className='text-sky-700'>Hello from Home!</h1>
+    <Button onClick={() => {
       logout()
-    }}>Logout</button></div>
+    }}>Logout</Button>
+</div>
   )
 }
