0e4f02f232edd6387b79741d049033b528b4815b
Tom <tomecrepont@gmail.com>
2024-12-17 10:12:50 +0000

    move base Api::V1 parent controller into a Api::V1::ApiV1Controller and setup error handling + pundit auth verification

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::API
-  include JwtAuthenticatable
-
-  before_action :authenticate_user
-
-  private
-
-  def current_user
-    @current_user
-  end
 end
