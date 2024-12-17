307a5d07a4adae5913c7fd053d30e460ecf9be9b
Tom <tomecrepont@gmail.com>
2024-12-17 14:02:03 +0000

    fix user and company factories

--- api/spec/factories/organization/companies.rb
+++ api/spec/factories/organization/companies.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :company, class: 'Organization::Company' do
-    name { "My company" }
-    registration_number { "123456789" }
-    email { "mycompany@example.com" }
+    name { "Company 1" }
+    sequence(:registration_number) { |n| "REG#{n.to_s.rjust(9, '0')}" }
+    sequence(:email) { |n| "company#{n}@example.com" }
     phone { "+33123456789" }
-    address_city { "15 rue des mouettes" }
-    address_street { "Biarritz" }
+    address_city { "Biarritz" }
+    address_street { "15 rue des mouettes" }
     address_zipcode { "64200" }
   end
 end
