c71808d4e25904e455e8515c65ab72a681f3c4b2
Tom <tomecrepont@gmail.com>
2024-12-17 16:56:57 +0000

    allow axios instance to access getAccessToken from useAuth by setting it in a global variable

--- frontend/src/contexts/auth_context.tsx
+++ frontend/src/contexts/auth_context.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect, ReactNode } from "react";
 import { useAuth } from "../hooks/use_auth";
+import { setGetAccessToken, setGetRefreshToken } from "@/lib/auth-service";
 
 type AuthContextType = {
   isAuthed: boolean;
@@ -9,48 +10,50 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType>(null);
 
 type AuthContextProviderProps = {
-  children: ReactNode
+  children: ReactNode;
 };
 
 const Provider = ({ children }: AuthContextProviderProps) => {
-    const { getAuthStatus } = useAuth();
-    console.log({ authStatus: getAuthStatus() });
-    const [isAuthed, setIsAuthed] = useState<boolean>(getAuthStatus());
-
-    return (
-        <AuthContext.Provider value={{ isAuthed, setIsAuthed }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-const Consumer = ({ children }: { children: ReactNode }) => {
-    const { setLocalStorageEventHandler, clearLocalStorageEventHandler } = useAuth();
-      useEffect(() => {
-        setLocalStorageEventHandler();
-        return clearLocalStorageEventHandler;
-      }, [setLocalStorageEventHandler, clearLocalStorageEventHandler]);
-
-    return (
-        <>
-            {children}
-        </>
-    )
-}
-
-
-const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-  
-
+  const { getAuthStatus } = useAuth();
+  console.log({ authStatus: getAuthStatus() });
+  const [isAuthed, setIsAuthed] = useState<boolean>(getAuthStatus());
 
   return (
+    <AuthContext.Provider value={{ isAuthed, setIsAuthed }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const Consumer = ({ children }: { children: ReactNode }) => {
+  const {
+    setLocalStorageEventHandler,
+    clearLocalStorageEventHandler,
+    getAccessToken,
+    getRefreshToken,
+  } = useAuth();
+  useEffect(() => {
+    setGetAccessToken(getAccessToken);
+    setGetRefreshToken(getRefreshToken);
+    setLocalStorageEventHandler();
+    return clearLocalStorageEventHandler;
+  }, [
+    setLocalStorageEventHandler,
+    clearLocalStorageEventHandler,
+    getAccessToken,
+    getRefreshToken,
+  ]);
+
+  return <>{children}</>;
+};
+
+const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  return (
     <Provider>
-        <Consumer>
-            {children}
-        </Consumer>
+      <Consumer>{children}</Consumer>
     </Provider>
   );
-}; 
+};
 
 export { AuthContext, AuthContextProvider };
 export type { AuthContextType };
---
+++ frontend/src/lib/auth-service.tsx
@@ -0,0 +1,13 @@
+let getAccessToken: () => string | null = () => null;
+
+const setGetAccessToken = (tokenFn: () => string | null) => {
+    getAccessToken = tokenFn;
+};
+
+let getRefreshToken: () => string | null = () => null;
+
+const setGetRefreshToken = (tokenFn: () => string | null) => {
+    getRefreshToken = tokenFn;
+};
+
+export {getAccessToken, setGetAccessToken, getRefreshToken, setGetRefreshToken}
