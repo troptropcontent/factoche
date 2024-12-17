0e8ab8b61871c36d8c48f65bea1dd2490f377bed
Tom <tomecrepont@gmail.com>
2024-12-17 16:59:08 +0000

    set context in root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,20 +1,24 @@
-import { createRootRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet, useNavigate } from "@tanstack/react-router";
 
 import React, { useEffect } from "react";
 import { useAuth } from "../hooks/use_auth";
+import { QueryClient } from "@tanstack/react-query";
+
+interface RouterContext {
+  queryClient: QueryClient
+}
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null // Render nothing in production
     : React.lazy(() =>
-        // Lazy load in development
         import("@tanstack/router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
         }))
       );
 
 const Root = () => {
-  const { isAuthed } = useAuth();
+  const { isAuthed} = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -31,6 +35,6 @@ const Root = () => {
   );
 };
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<RouterContext>()({
   component: Root,
-});
+})
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { useAuth } from '../hooks/use_auth'
+import { Button } from '@/components/ui/button'
+import { Layout } from '@/components/layout/layout'
+import { getCompaniesQueryOptions } from '@/queries/organization/companies/getCompaniesQueryOptions'
+
+export const Route = createFileRoute('/')({
+  loader: ({ context: { queryClient } }) =>
+    queryClient.ensureQueryData(getCompaniesQueryOptions),
+  component: Index,
+})
+
+function Index() {
+  const { logout } = useAuth()
+  const data = Route.useLoaderData()
+  console.log({data})
+  return (
+    <Layout>
+      <div>
+        <h1 className="text-sky-700">Hello from Home!</h1>
+        <Button
+          onClick={() => {
+            logout()
+          }}
+        >
+          Logout
+        </Button>
+      </div>
+    </Layout>
+  )
+}
