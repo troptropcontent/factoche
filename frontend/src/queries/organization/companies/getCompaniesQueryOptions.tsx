7a6234af3bdc72ab5a4ca4c7eb621e7105f3366d
Tom <tomecrepont@gmail.com>
2024-12-17 16:59:39 +0000

    create getCompaniesQueryOptions

---
+++ frontend/src/queries/organization/companies/getCompaniesQueryOptions.tsx
@@ -0,0 +1,9 @@
+import { api } from "@/services/api";
+import { queryOptions } from "@tanstack/react-query";
+
+const getCompaniesQueryOptions = queryOptions({
+  queryKey: ["organization", "companies"],
+  queryFn: api.organization.companies.index,
+});
+
+export { getCompaniesQueryOptions };
