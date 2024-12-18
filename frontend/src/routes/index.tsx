a1e8a02cb6ee5bbad95f7536420692ff4f0e3c3a
Tom <tomecrepont@gmail.com>
2024-12-18 09:53:26 +0000

    create a simple route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,5 @@
-import { createFileRoute, Navigate } from "@tanstack/react-router";
-import { useAuth } from "../hooks/use_auth";
-import { Button } from "@/components/ui/button";
-import { Layout } from "@/components/layout/layout";
+import { createFileRoute } from "@tanstack/react-router";
+
 import { getCompaniesQueryOptions } from "@/queries/organization/companies/getCompaniesQueryOptions";
 
 export const Route = createFileRoute("/")({
@@ -11,28 +9,9 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  const { logout } = useAuth();
-  const data = Route.useLoaderData();
-
-  const companyId = data[0]?.id;
-
-  return companyId ? (
-    <Navigate
-      to="/organization/companies/$companyId/clients"
-      params={{ companyId: companyId.toString() }}
-    />
-  ) : (
-    <Layout companyId={companyId.toString()}>
-      <div>
-        <h1 className="text-sky-700">Hello from Home!</h1>
-        <Button
-          onClick={() => {
-            logout();
-          }}
-        >
-          Logout
-        </Button>
-      </div>
-    </Layout>
+  return (
+    <div>
+      <h1 className="text-sky-700">Hello from Home!</h1>
+    </div>
   );
 }
