40137be11931e0926cc80b9e5458cbd9f8f84928
Tom <tomecrepont@gmail.com>
2025-01-15 17:45:34 +0000

    create api path for create project controller

--- api/app/controllers/api/v1/organization/projects_controller.rb
+++ api/app/controllers/api/v1/organization/projects_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::Organization::ProjectsController < Api::V1::ApiV1Controller
   before_action :load_company!
+
   # POST /api/v1/organization/companies/:company_id/projects
   def create
-    head :ok
+    project = Organization::CreateProject.call(create_project_dto)
+    render json: serialize_project(project)
   end
 
   private
@@ -10,5 +12,94 @@ class Api::V1::Organization::ProjectsController < Api::V1::ApiV1Controller
   def load_company!
     @company = policy_scope(Organization::Company).find_by(id: params[:company_id])
     raise Error::NotFoundError unless @company
+  end
+
+  def client_id_params
+    params.require(:project).require(:client_id)
+  end
+
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :description,
+      :retention_guarantee_rate,
+      :client_id,
+      items: [
+        :name,
+        :description,
+        :position,
+        :quantity,
+        :unit,
+        :unit_price_cents,
+        items: [
+          :name,
+          :description,
+          :position,
+          :quantity,
+          :unit,
+          :unit_price_cents
+        ]
+      ]
+    )
+  end
+
+  def create_project_dto
+    Organization::CreateProjectDto.new(project_params)
+  end
+
+  def serialize_project(project)
+    Organization::ProjectDto.new(project_dto_params(project)).to_json
+  end
+
+  def project_dto_params(project)
+    {
+      id: project.id,
+      name: project.name,
+      client_id: project.client_id,
+      description: project.description,
+      versions: project.versions.map { |version| {
+        id: version.id,
+        number: version.number,
+        retention_rate_guarantee: version.retention_guarantee_rate,
+        items: items_params(version)
+      }}
+    }
+  end
+
+  def items_params(project_version)
+    item_groups = project_version.item_groups
+    if item_groups.length > 0
+      item_groups.map { |item_group| {
+        id: item_group.id,
+        name: item_group.name,
+        description: item_group.description,
+        position: item_group.position,
+        items: item_group.items.map { |item| {
+          id: item.id,
+          name: item.name,
+          quantity: item.quantity,
+          unit_price_cents: item.unit_price_cents,
+          unit: item.unit,
+          position: item.position
+        }}
+      }}
+    else
+      project_version.items.map { |item| {
+        id: item.id,
+        name: item.name,
+        quantity: item.quantity,
+        unit_price_cents: item.unit_price_cents,
+        unit: item.unit,
+        position: item.position
+      }}
+    end
+  end
+
+  def create_project_dto
+    Organization::CreateProjectDto.new(project_params)
+  end
+
+  def serialize_project(project)
+    Organization::ProjectDto.new(project_dto_params(project)).to_json
   end
 end
--- api/spec/controllers/api/v1/organization/projects_controller_spec.rb
+++ api/spec/controllers/api/v1/organization/projects_controller_spec.rb
@@ -1,59 +1,104 @@
 require "rails_helper"
 require "swagger_helper"
 
-RSpec.describe Api::V1::Organization::ProjectsController, type: :request do
+RSpec.describe Api::V1::Organization::ProjectsController, type: :request, focus: true  do
   path "/api/v1/organization/companies/{company_id}/projects" do
     post "Creates a new project and its descendants" do
-      tags 'Projects'
+      tags "Projects"
       security [ bearer_auth: [] ]
-      consumes 'application/json'
-      produces 'application/json'
+      consumes "application/json"
+      produces "application/json"
       parameter name: :company_id, in: :path, type: :integer
-      parameter name: :project, in: :body, schema: { oneOf: [
-        { '$ref' => '#/components/schemas/create_project_with_item_groups' },
-        { '$ref' => '#/components/schemas/create_project_with_items' }
-      ] }
+      parameter name: :project, in: :body, schema: Organization::CreateProjectDto.to_schema
 
-      response '200', 'project created', focus: true do
+      response "200", "project created" do
         let(:user) { FactoryBot.create(:user) }
         let(:company) { FactoryBot.create(:company) }
         let!(:client) { FactoryBot.create(:client, company: company) }
         let!(:member) { FactoryBot.create(:member, user:, company:) }
         let(:company_id) { company.id }
         let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(user.id)}" }
+        schema Organization::ProjectDto.to_schema
+
         describe "simple project, without item groups" do
-          let(:project) {
-          {
-            name: "Amazing Project",
-            project_version_attributes: {
-              retention_guarantee_rate: 500,
-              items_attributes: [
-                { name: "Screws", unit: "ENS", unit_price: 1500, quantity: 4 },
-                { name: "Bolts", unit: "ENS", unit_price: 2000, quantity: 8 }
-              ]
-            }
-          }
-        }
-          run_test!
-        end
-        describe "advanced project, with item groups" do
-          let(:project) {
+          let(:project) do
             {
               name: "Amazing Project",
-              project_version_attributes: {
-                retention_guarantee_rate: 500,
-                item_groups_attributes: {
-                  name: "Building",
-                  items_attributes: [
-                    { name: "Screws", unit: "ENS", unit_price: 1500, quantity: 4 },
+              client_id: client.id,
+              retention_guarantee_rate: 5,
+              items: [
+                {
+                  name: "Screws",
+                  unit: "ENS",
+                  position: 1,
+                  unit_price_cents: 1500,
+                  quantity: 4
+                },
+                {
+                  name: "Bolts",
+                  unit: "ENS",
+                  position: 2,
+                  unit_price_cents: 2000,
+                  quantity: 8
+                }
+              ]
+            }
+          end
+          let!(:number_of_project_before) { Organization::Project.count }
+          let!(:number_of_project_version_before) { Organization::ProjectVersion.count }
+          let!(:number_of_items_before) { Organization::Item.count }
+          let!(:number_of_item_groups_before) { Organization::ItemGroup.count }
 
-                    { name: "Bolts", unit: "ENS", unit_price: 2000, quantity: 8 }
+          run_test! "create a new project with the relevant descendants" do
+            expect(Organization::Project.count).to eq(number_of_project_before + 1)
+            expect(Organization::ProjectVersion.count).to eq(number_of_project_version_before + 1)
+            expect(Organization::Item.count).to eq(number_of_items_before + 2)
+            expect(Organization::ItemGroup.count).to eq(number_of_item_groups_before + 0)
+          end
+        end
+
+        describe "advanced project, with item groups" do
+          let(:project) do
+            {
+              name: "Amazing Project",
+              client_id: client.id,
+              retention_guarantee_rate: 5,
+              items: [
+                {
+                  name: "Building",
+                  position: 1,
+                  items: [
+                    {
+                      name: "Screws",
+                      unit: "ENS",
+                      position: 1,
+                      unit_price_cents: 1500,
+                      quantity: 4
+                    },
+                    {
+                      name: "Bolts",
+                      unit: "ENS",
+                      position: 2,
+                      unit_price_cents: 2000,
+                      quantity: 8
+                    }
                   ]
                 }
-              }
+              ]
             }
-          }
-          run_test!
+          end
+
+          let!(:number_of_project_before) { Organization::Project.count }
+          let!(:number_of_project_version_before) { Organization::ProjectVersion.count }
+          let!(:number_of_items_before) { Organization::Item.count }
+          let!(:number_of_item_groups_before) { Organization::ItemGroup.count }
+
+          run_test! "create a new project with the relevant descendants" do
+            expect(Organization::Project.count).to eq(number_of_project_before + 1)
+            expect(Organization::ProjectVersion.count).to eq(number_of_project_version_before + 1)
+            expect(Organization::Item.count).to eq(number_of_items_before + 2)
+            expect(Organization::ItemGroup.count).to eq(number_of_item_groups_before + 1)
+          end
         end
       end
     end
