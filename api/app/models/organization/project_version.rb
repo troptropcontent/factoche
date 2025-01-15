a3fe54610880859606fe239d3c53a4a9f6401e90
Tom <tomecrepont@gmail.com>
2025-01-15 09:46:49 +0000

    fix project version

--- api/app/models/organization/project_version.rb
+++ api/app/models/organization/project_version.rb
@@ -7,13 +7,15 @@ class Organization::ProjectVersion < ApplicationRecord
   has_many :item_groups, class_name: "Organization::ItemGroup"
   accepts_nested_attributes_for :item_groups
 
+  validates :retention_guarantee_rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10000 }
+
   validates :number, presence: true, uniqueness: { scope: :project_id }
   before_validation :set_number_to_next_available_number, on: :create
 
   private
 
   def next_available_number
-    raise "Project must be set to determine next version number" unless project_id
+    raise "Project must be set to determine next version number" unless project
     project.project_versions.maximum(:number).to_i + 1
   end
 
