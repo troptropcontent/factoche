37091ff7331808407d41b510bd7d668fe33dc369
Tom <tomecrepont@gmail.com>
2025-01-15 09:47:53 +0000

    remove retnetion_guarantee_rate from project

--- api/app/models/organization/project.rb
+++ api/app/models/organization/project.rb
@@ -4,5 +4,4 @@ class Organization::Project < ApplicationRecord
   accepts_nested_attributes_for :project_versions
 
   validates :name, presence: true, uniqueness: { scope: :client_id }
-  validates :retention_guarantee_rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
