1aa4598486519a82f7a5444bf8c9d3d689db781d
Tom <tomecrepont@gmail.com>
2025-01-24 14:56:30 +0000

    use the new method in ProjectVersionsController

--- api/app/controllers/api/v1/organization/project_versions_controller.rb
+++ api/app/controllers/api/v1/organization/project_versions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Organization::ProjectVersionsController < Api::V1::ApiV1Controller
-  before_action :load_and_authorise_company!
+  before_action { load_and_authorise_resource(:company, class_name: "Organization::Company") }
   # GET /api/v1/organization/companies/{company_id}/projects/{project_id}/versions
   def index
     versions = policy_scope(Organization::ProjectVersion).where({ project: { organization_clients: { company_id: @company.id  }, id: params[:project_id] } })
@@ -12,13 +12,5 @@ class Api::V1::Organization::ProjectVersionsController < Api::V1::ApiV1Controlle
     version = policy_scope(Organization::ProjectVersion).where({ project: { organization_clients: { company_id: @company.id  }, id: params[:project_id] } }).find(params[:id])
 
     render json: Organization::ProjectVersionShowResponseDto.new({ result: version }).to_json
-  end
-
-  private
-
-  def load_and_authorise_company!
-    @company = Organization::Company.find(params[:company_id])
-
-    raise Error::UnauthorizedError unless policy_scope(Organization::Company).exists?({ id: @company.id })
   end
 end
