d54b482885d7023fc278ec2fa97c7f1f76d02e32
Tom <tomecrepont@gmail.com>
2025-01-27 15:03:52 +0000

    create an authenticated endpoint shared example

---
+++ api/spec/controllers/api/v1/organization/shared_examples/an_authenticated_endpoint.rb
@@ -0,0 +1,23 @@
+RSpec.shared_examples "an authenticated endpoint" do |parameter|
+  response "403", "forbiden" do
+    describe "when the token is not there" do
+      let(:Authorization) { "" }
+
+      run_test!
+    end
+
+    describe "when the token is not valid" do
+      let(:Authorization) { "q" }
+
+      run_test!
+    end
+
+    describe "when the token is expired" do
+      let(:Authorization) {
+        travel_to 3.day.before { "Bearer #{JwtAuth.generate_access_token(user.id)}"  }
+      }
+
+      run_test!
+    end
+  end
+end
