257662fcced0fa007c032d575f3e85bc45745142
Tom <tomecrepont@gmail.com>
2025-01-27 20:07:48 +0000

    release the endpoint

--- api/app/controllers/api/v1/organization/completion_snapshots_controller.rb
+++ api/app/controllers/api/v1/organization/completion_snapshots_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::Organization::CompletionSnapshotsController < Api::V1::ApiV1Controller
-  before_action { load_and_authorise_resource(:project, class_name: "Organization::Project") }
+  before_action(only: :create) { load_and_authorise_resource(:project, class_name: "Organization::Project") }
   # POST /api/v1/organization/companies/:company_id/projects/:project_id/completion_snapshots
   def create
     dto = Organization::CreateCompletionSnapshotDto.new(completion_snapshot_params)
     completion_snapshot = Organization::CreateCompletionSnapshot.call(dto, params[:project_id])
 
     render json: Organization::ShowCompletionSnapshotResponseDto.new({ result: completion_snapshot }).to_json
+  end
+
+  # GET  /api/v1/organization/completion_snapshots/:id
+  def show
+    snapshot = policy_scope(Organization::CompletionSnapshot).find(params[:id])
+
+    render json: Organization::ShowCompletionSnapshotResponseDto.new({ result: snapshot }).to_json
   end
 
   private
