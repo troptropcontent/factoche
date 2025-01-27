4e7d69a1f67a1b0f5e8535d5c405d484306f612d
Tom <tomecrepont@gmail.com>
2025-01-27 09:18:54 +0000

    preload version data

--- frontend/src/routes/_authenticated/companies/$companyId/projects/$projectId/index.tsx
+++ frontend/src/routes/_authenticated/companies/$companyId/projects/$projectId/index.tsx
@@ -1,21 +1,65 @@
 import { Layout } from "@/components/pages/companies/layout";
+import { ProjectShowContent } from "@/components/pages/companies/projects/show/project-show-content";
+import { Api } from "@/lib/openapi-fetch-query-client";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(
   "/_authenticated/companies/$companyId/projects/$projectId/"
 )({
   component: RouteComponent,
+  loader: ({ context: { queryClient }, params: { companyId, projectId } }) =>
+    queryClient
+      .ensureQueryData(
+        Api.queryOptions(
+          "get",
+          "/api/v1/organization/companies/{company_id}/projects/{id}",
+          {
+            params: {
+              path: { company_id: Number(companyId), id: Number(projectId) },
+            },
+          }
+        )
+      )
+      .then(async (projectData) => {
+        await queryClient.ensureQueryData(
+          Api.queryOptions(
+            "get",
+            "/api/v1/organization/companies/{company_id}/projects/{project_id}/versions/{id}",
+            {
+              params: {
+                path: {
+                  company_id: Number(companyId),
+                  project_id: Number(projectId),
+                  id: projectData.result.last_version.id,
+                },
+              },
+            }
+          )
+        );
+
+        return projectData;
+      }),
 });
 
 function RouteComponent() {
+  const { result: project } = Route.useLoaderData();
+  const { companyId, projectId } = Route.useParams();
+
   return (
     <Layout.Root>
       <Layout.Header>
         <div className="flex flex-grow justify-between items-center">
-          <h1 className="text-3xl font-bold">Project Show Title</h1>
+          <h1 className="text-3xl font-bold">{project.name}</h1>
         </div>
       </Layout.Header>
-      <Layout.Content>Project Show Content</Layout.Content>
+      <Layout.Content>
+        <ProjectShowContent
+          companyId={Number(companyId)}
+          projectId={Number(projectId)}
+          client={project.client}
+          initialVersionId={project.last_version.id}
+        />
+      </Layout.Content>
     </Layout.Root>
   );
 }
