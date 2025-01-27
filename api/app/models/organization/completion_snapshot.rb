f9eafba004a1ac0b6a397dcf05bc1e737bb50655
Tom <tomecrepont@gmail.com>
2025-01-27 15:06:09 +0000

    remove validation from the model, those validation should be there for data consistency not integrity

--- api/app/models/organization/completion_snapshot.rb
+++ api/app/models/organization/completion_snapshot.rb
@@ -2,19 +2,10 @@ class Organization::CompletionSnapshot < ApplicationRecord
   belongs_to :project_version, class_name: "Organization::ProjectVersion"
   has_many :completion_snapshot_items, class_name: "Organization::CompletionSnapshotItem"
   accepts_nested_attributes_for :completion_snapshot_items
+  belongs_to :invoice, class_name: "Organization::Invoice", optional: true
+  belongs_to :credit_note, class_name: "Organization::CreditNote", optional: true
 
   scope :draft, -> { where("invoice_id IS NULL") }
   scope :invoiced, -> { where("invoice_id IS NOT NULL AND credit_note_id IS NULL") }
   scope :cancelled, -> { where("invoice_id IS NOT NULL AND credit_note_id IS NOT NULL") }
-
-  validate :only_one_new_completion_snapshot_per_project
-
-  private
-
-  def only_one_new_completion_snapshot_per_project
-    true
-    # if project_version.project.completion_snapshots.draft && expiration_date < Date.today
-    #   errors.add(:base, "can't be in the past")
-    # end
-  end
 end
