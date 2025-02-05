339010e2c783cdb9af7d5a653625dc6d5d6d5126
Tom <tomecrepont@gmail.com>
2025-02-05 10:25:04 +0000

    add specs for completion_snapshot.status

--- api/spec/models/organization/completion_snapshot_spec.rb
+++ api/spec/models/organization/completion_snapshot_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization::CompletionSnapshot, type: :model do
-  subject { FactoryBot.create(:completion_snapshot, project_version: project_version) }
+  subject(:completion_snapshot) { FactoryBot.create(:completion_snapshot, project_version: project_version) }
 
   let(:company) { FactoryBot.create(:company) }
   let(:client) { FactoryBot.create(:client, company: company) }
@@ -11,5 +11,31 @@ RSpec.describe Organization::CompletionSnapshot, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:project_version).class_name('Organization::ProjectVersion') }
     it { is_expected.to have_many(:completion_snapshot_items).class_name('Organization::CompletionSnapshotItem') }
+  end
+
+  describe "instance methods" do
+    describe "#status" do
+      context "when neither an invoice nor a credit note is attached" do
+        it "returns draft" do
+          expect(completion_snapshot.status).to eq("draft")
+        end
+      end
+
+      context "when an invoice_id is there but no credit_note" do
+        before { completion_snapshot.update({ invoice: FactoryBot.create(:invoice) }) }
+
+        it "returns invoiced" do
+          expect(completion_snapshot.status).to eq("invoiced")
+        end
+      end
+
+      context "when a credit_note is there" do
+        before { completion_snapshot.update({ invoice: FactoryBot.create(:invoice), credit_note:  FactoryBot.create(:credit_note) }) }
+
+        it "returns cancelled" do
+          expect(completion_snapshot.status).to eq("cancelled")
+        end
+      end
+    end
   end
 end
