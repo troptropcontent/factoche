9b551f2b3823b0d15326d19d7dde1630ae2b50d4
Tom <tomecrepont@gmail.com>
2025-02-12 15:27:23 +0000

    update shared_context 'a company with a project with three item groups' to allow overwrites of quantoties and unoit_prices

--- api/spec/support/shared_contexts/organization/a_company_with_a_project_with_three_item_groups.rb
+++ api/spec/support/shared_contexts/organization/a_company_with_a_project_with_three_item_groups.rb
@@ -7,13 +7,20 @@ RSpec.shared_context 'a company with a project with three item groups' do
   let(:client) { FactoryBot.create(:client, company: company) }
   let(:project) { FactoryBot.create(:project, client: client) }
   let(:project_version) { FactoryBot.create(:project_version, project: project) }
-  [ "first", "second", "third" ].each do |ordinal|
+  [ "first", "second", "third" ].each_with_index do |ordinal, index|
     let("project_version_#{ordinal}_item_group") do
-      FactoryBot.create(:item_group, project_version: project_version)
-    end
+    FactoryBot.create(:item_group, project_version: project_version)
+  end
+    let("project_version_#{ordinal}_item_group_item_unit_price_cents") { 100 * (index + 1) }
+    let("project_version_#{ordinal}_item_group_item_unit_quantity") { index + 1 }
     # rubocop:disable RSpec/LetSetup
     let!("project_version_#{ordinal}_item_group_item") do
-      FactoryBot.create(:item, project_version: project_version, item_group: send("project_version_#{ordinal}_item_group"))
+      FactoryBot.create(
+        :item,
+        project_version: project_version,
+        item_group: send("project_version_#{ordinal}_item_group"),
+        quantity: send("project_version_#{ordinal}_item_group_item_unit_quantity"),
+        unit_price_cents: send("project_version_#{ordinal}_item_group_item_unit_price_cents"))
     end
   end
 end
