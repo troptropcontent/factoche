e535bc2f6fcd306321a0be67db5a5ad5f61acf03
Tom <tomecrepont@gmail.com>
2025-02-17 09:18:19 +0000

    setup sidekiq

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,5 +28,13 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # To enable sidekiq web
+    config.session_store :cookie_store, key: "_your_app_session"
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
   end
 end
