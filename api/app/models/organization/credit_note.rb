e4fb1544e5112e28ecaf00922475a6cfe32871e7
Tom <tomecrepont@gmail.com>
2025-02-21 14:14:07 +0000

    remove useless assocaiton

--- api/app/models/organization/credit_note.rb
+++ api/app/models/organization/credit_note.rb
@@ -1,5 +1,4 @@
 module Organization
   class CreditNote < Organization::AccountingDocument
-    has_one :completion_snapshot, class_name: "Organization::CompletionSnapshot", foreign_key: :credit_note_id
   end
 end
--- api/app/models/organization/invoice.rb
+++ api/app/models/organization/invoice.rb
@@ -1,7 +1,5 @@
 module Organization
   class Invoice < Organization::AccountingDocument
-    has_one :completion_snapshot, class_name: "Organization::CompletionSnapshot", foreign_key: :invoice_id
-
     def rebuild_payload
       raise Error::UnprocessableEntityError.new("Can only rebuild payload in development environment") unless Rails.env.development?
       self.update(payload: BuildCompletionSnapshotInvoicePayload.call(completion_snapshot, issue_date))
