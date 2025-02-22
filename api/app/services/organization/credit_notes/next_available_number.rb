75e1d513e6e19a5e503db3146813ca985adbf360
Tom <tomecrepont@gmail.com>
2025-02-22 14:35:08 +0000

    include the issue_date year into the number

--- api/app/services/organization/credit_notes/next_available_number.rb
+++ api/app/services/organization/credit_notes/next_available_number.rb
@@ -2,10 +2,12 @@ module Organization
   module CreditNotes
     class NextAvailableNumber
       class << self
-        def call(company)
-          credit_notes_count = company.credit_notes.count
+        def call(company, issue_date)
+          year = issue_date.year
+          credit_note_count = company.credit_notes.where(issue_date: issue_date.beginning_of_year..issue_date.end_of_year).count
 
-          "CN-#{(credit_notes_count + 1).to_s.rjust(6, "0")}"
+
+          "CN-#{year}-#{(credit_note_count + 1).to_s.rjust(6, "0")}"
         end
       end
     end
--- api/spec/services/organization/credit_notes/next_available_number_spec.rb
+++ api/spec/services/organization/credit_notes/next_available_number_spec.rb
@@ -6,21 +6,25 @@ RSpec.describe Organization::CreditNotes::NextAvailableNumber do
     include_context 'a company with a project with three item groups'
 
     context 'when there are no existing credit notes' do
-      it 'returns CN-000001' do
-        expect(described_class.call(company)).to eq('CN-000001')
+      it 'returns CN-YEAR-000001' do
+        travel_to(Time.new(2024, 6, 6)) do
+          expect(described_class.call(company, Time.current)).to eq('CN-2024-000001')
+        end
       end
     end
 
     context 'when there are existing credit notes' do
+      let(:issue_date) { Time.new(2024, 6, 6) }
+
       before do
         3.times do
-          completion_snapshot = FactoryBot.create(:completion_snapshot, :with_invoice, project_version: project_version)
-          FactoryBot.create(:credit_note, original_invoice: completion_snapshot.invoice)
+          completion_snapshot = FactoryBot.create(:completion_snapshot, :with_invoice, project_version: project_version, invoice_issue_date: issue_date)
+          FactoryBot.create(:credit_note, original_invoice: completion_snapshot.invoice, issue_date: issue_date)
         end
       end
 
       it 'returns the next available number' do
-        expect(described_class.call(company)).to eq('CN-000004')
+        expect(described_class.call(company, issue_date)).to eq('CN-2024-000004')
       end
     end
   end
