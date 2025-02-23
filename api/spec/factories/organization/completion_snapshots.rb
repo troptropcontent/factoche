9308c4157624b0df0caa0403d71b1c16e419ca8a
Tom <tomecrepont@gmail.com>
2025-02-23 13:33:29 +0000

    implement an invoice_issue_date transient attribute into colpletion_snaptshot factory to be able to pass an issue date to the Organization::BuildInvoiceFromCompletionSnapshot

--- api/spec/factories/organization/completion_snapshots.rb
+++ api/spec/factories/organization/completion_snapshots.rb
@@ -2,9 +2,12 @@ FactoryBot.define do
   factory :completion_snapshot, class: 'Organization::CompletionSnapshot' do
     project_version { nil }
     description { "MyString" }
+    transient do
+      invoice_issue_date { Time.current }
+    end
     trait :with_invoice do
-      after(:create) do |completion_snapshot|
-        Organization::BuildInvoiceFromCompletionSnapshot.call(completion_snapshot, Time.current).save!
+      after(:create) do |completion_snapshot, evaluator|
+        Organization::BuildInvoiceFromCompletionSnapshot.call(completion_snapshot, evaluator.invoice_issue_date).save!
         completion_snapshot.reload
       end
     end
