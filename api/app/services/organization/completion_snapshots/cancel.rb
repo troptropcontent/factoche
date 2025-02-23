541a894eab8f501dffc3d7962725b5a915b2a2d3
Tom <tomecrepont@gmail.com>
2025-02-23 19:56:28 +0000

    directly create published credit note

--- api/app/services/organization/completion_snapshots/cancel.rb
+++ api/app/services/organization/completion_snapshots/cancel.rb
@@ -4,14 +4,15 @@ module Organization
       class << self
         def call(snapshot)
           ensure_snapshot_is_published!(snapshot)
-
+          credit_note = nil
           ActiveRecord::Base.transaction do
             snapshot.invoice.update!({ status: :cancelled })
             credit_note = CreditNotes::BuildCreditNoteFromInvoice.call(snapshot.invoice)
+            credit_note.status = :published
             credit_note.save!
-
-            ServiceResult.success(credit_note)
           end
+          # Trigger the generation of the credit_note here
+          ServiceResult.success(credit_note)
         rescue StandardError => e
           ServiceResult.failure(e)
         end
