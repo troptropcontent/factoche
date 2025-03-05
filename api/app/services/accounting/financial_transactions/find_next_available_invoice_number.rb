0f9d862c29b659ef230b43eaf1b1b5d50c3cbe23
Tom <tomecrepont@gmail.com>
2025-03-05 09:40:36 +0000

    FindNextAvailableInvoiceNumber

---
+++ api/app/services/accounting/financial_transactions/find_next_available_invoice_number.rb
@@ -0,0 +1,18 @@
+module Accounting
+  module FinancialTransactions
+    class FindNextAvailableInvoiceNumber
+      INVOICE_PREFIX = "INV".freeze
+      class << self
+        def call(company_id, issue_date = Time.current)
+          invoice_count = FinancialTransaction.posted.where("type LIKE '%Invoice' AND company_id = ?", company_id).count
+
+          new_number = [ INVOICE_PREFIX, issue_date.year, (invoice_count + 1).to_s.rjust(6, "0") ].join("-")
+          # TODO: Implement logic to find next available invoice number
+          ServiceResult.success(new_number)
+        rescue StandardError => e
+          ServiceResult.failure("Failed to find next available invoice number: #{e.message}")
+        end
+      end
+    end
+  end
+end
