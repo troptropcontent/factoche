bc9429db6cfd30b7fdc514931a82ab6d31777d25
Tom <tomecrepont@gmail.com>
2025-03-11 09:34:13 +0000

    release PUT  /api/v1/organization/projects/:project_id/invoices/:id

--- api/app/controllers/api/v1/organization/invoices_controller.rb
+++ api/app/controllers/api/v1/organization/invoices_controller.rb
@@ -1,27 +1,54 @@
-class Api::V1::Organization::InvoicesController < Api::V1::ApiV1Controller
-  # GET /api/v1/organization/projects/:project_id/invoices
-  def index
-    project = policy_scope(Organization::Project).find(params[:project_id])
+module Api
+  module V1
+    module Organization
+      class InvoicesController < ApiV1Controller
+        # GET /api/v1/organization/projects/:project_id/invoices
+        def index
+          project = policy_scope(::Organization::Project).find(params[:project_id])
 
-    invoices = Accounting::CompletionSnapshotInvoice
-      .joins(:lines)
-      .select(
-        "accounting_financial_transactions.*, " \
-        "SUM(accounting_financial_transaction_lines.quantity * " \
-        "accounting_financial_transaction_lines.unit_price_amount) as total_amount"
-      )
-      .where(holder_id: project.versions.pluck(:id))
-      .group("accounting_financial_transactions.id")
+          invoices = Accounting::CompletionSnapshotInvoice
+            .joins(:lines)
+            .select(
+              "accounting_financial_transactions.*, " \
+              "SUM(accounting_financial_transaction_lines.quantity * " \
+              "accounting_financial_transaction_lines.unit_price_amount) as total_amount"
+            )
+            .where(holder_id: project.versions.pluck(:id))
+            .group("accounting_financial_transactions.id")
 
-    render json: ::Organization::Invoices::IndexDto.new({ results: invoices })
-  end
+          render json: ::Organization::Invoices::IndexDto.new({ results: invoices })
+        end
 
-  # GET  /api/v1/organization/projects/:project_id/invoices/:id
-  def show
-    project = policy_scope(Organization::Project).find(params[:project_id])
+        # GET  /api/v1/organization/projects/:project_id/invoices/:id
+        def show
+          project = policy_scope(::Organization::Project).find(params[:project_id])
 
-    invoice = Accounting::CompletionSnapshotInvoice.where(holder_id: project.versions.pluck(:id)).includes(:lines).find(params[:id])
+          invoice = Accounting::CompletionSnapshotInvoice.where(holder_id: project.versions.pluck(:id)).includes(:lines).find(params[:id])
 
-    render json: ::Organization::Invoices::ShowDto.new({ result: invoice })
+          render json: ::Organization::Invoices::ShowDto.new({ result: invoice })
+        end
+
+        # PUT  /api/v1/organization/projects/:project_id/invoices/:id
+        def update
+          project = policy_scope(::Organization::Project).find(params[:project_id])
+
+          invoice = Accounting::CompletionSnapshotInvoice.where(holder_id: project.versions.pluck(:id)).includes(:lines).find(params[:id])
+
+          result = ::Organization::Invoices::Update.call(invoice.id, update_params.to_h)
+
+          if result.failure?
+            raise Error::UnprocessableEntityError, "Failed to update invoice: #{result.error}"
+          end
+
+          render json: ::Organization::Invoices::ShowDto.new({ result: result.data })
+        end
+
+        private
+
+        def update_params
+          params.require(:invoice).permit(invoice_amounts: [ :original_item_uuid, :invoice_amount ])
+        end
+      end
+    end
   end
 end
--- api/spec/controllers/api/v1/organization/invoices_controller_spec.rb
+++ api/spec/controllers/api/v1/organization/invoices_controller_spec.rb
@@ -75,5 +75,99 @@ RSpec.describe Api::V1::Organization::InvoicesController, type: :request do
 
       it_behaves_like "an authenticated endpoint"
     end
+    put 'Update invoice' do
+      tags 'Invoices'
+      security [ bearerAuth: [] ]
+      produces 'application/json'
+      consumes 'application/json'
+      parameter name: :project_id, in: :path, type: :integer
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :body, in: :body, required: true, schema: {
+        type: :object,
+        required: [ :invoice_amounts ],
+        properties: {
+          invoice_amounts: {
+            type: :array,
+            items: {
+              type: :object,
+              required: [ :original_item_uuid, :invoice_amount ],
+              properties: {
+                original_item_uuid: { type: :string },
+                invoice_amount: { type: :number }
+              }
+            }
+          }
+        }
+      }
+
+      let!(:invoice) {
+        ::Organization::Invoices::CompletionSnapshots::Create.call(project_version.id, { invoice_amounts: [ { original_item_uuid: first_item.original_item_uuid, invoice_amount: "0.2" }, { original_item_uuid: second_item.original_item_uuid, invoice_amount: "0.2" } ] }).data
+      }
+      let(:id) { invoice.id }
+      let(:project_id) { project.id }
+      let(:user) { FactoryBot.create(:user) }
+      let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(user.id)}" }
+      let(:body) { { invoice_amounts: [ { original_item_uuid: first_item.original_item_uuid, invoice_amount: "1" } ] } }
+      include_context 'a company with a project with three items'
+
+      response '200', 'invoice updated' do
+        let!(:member) { FactoryBot.create(:member, user:, company:) }
+        schema Organization::Invoices::ShowDto.to_schema
+
+        it "Updates the invoice" do |example|
+          expect { submit_request(example.metadata) }
+            .to change { invoice.reload.lines.sum('excl_tax_amount') }.from(0.4).to(1)
+
+          assert_response_matches_metadata(example.metadata)
+        end
+
+        context "when the client have changed" do
+          before { client.update(name: "New Client Name") }
+
+          it "Updates the invoice details accordingly" do |example|
+            expect { submit_request(example.metadata) }
+              .to change { invoice.reload.detail.client_name }.from("Super Client").to("New Client Name")
+
+            assert_response_matches_metadata(example.metadata)
+          end
+        end
+      end
+
+      response '404', 'invoice not found' do
+        describe "when the project does npot belong to a company the user is a member of" do
+          run_test!
+        end
+      end
+
+      response '422', 'unprocessable entity' do
+        let!(:member) { FactoryBot.create(:member, user:, company:) }
+
+        context "when the invoice is not draftf" do
+          before { invoice.update(status: :posted, number: "INV-000") }
+
+          run_test!
+        end
+
+        context "when the invoice_amount would exceed the total amount allowed in the project for the item" do
+          let(:body) { { invoice_amounts: [ { original_item_uuid: first_item.original_item_uuid, invoice_amount: "100000" } ] } }
+
+          run_test!
+        end
+
+        context "when the original_item_item does not belong to the project" do
+          let(:body) { { invoice_amounts: [ { original_item_uuid: "antoher_id", invoice_amount: "1" } ] } }
+
+          run_test!
+        end
+
+        context "when the params are not valid" do
+          let(:body) { { invoice_amounts: [ { invoice_amount: "1" } ] } }
+
+          run_test!
+        end
+      end
+
+      it_behaves_like "an authenticated endpoint"
+    end
   end
 end
