bfc1ffc29a1967a7677445bc3aa9ac748af7c666
Tom <tomecrepont@gmail.com>
2025-03-11 09:32:25 +0000

    does not allow empty invoice amounyts

--- api/app/contracts/organization/invoices/completion_snapshots/create_contract.rb
+++ api/app/contracts/organization/invoices/completion_snapshots/create_contract.rb
@@ -3,7 +3,7 @@ module Organization
     module CompletionSnapshots
       class CreateContract < Dry::Validation::Contract
         params do
-          required(:invoice_amounts).array(:hash) do
+          required(:invoice_amounts).filled(:array).array(:hash) do
             required(:original_item_uuid).filled(:string)
             required(:invoice_amount).filled(:decimal)
           end
