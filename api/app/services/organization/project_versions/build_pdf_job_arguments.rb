0b149ac4562eea246c114439d0d743deafc31e05
Tom <tomecrepont@gmail.com>
2025-03-21 10:14:43 +0000

    create Organization::ProjectVersions::BuildPdfJobArguments

---
+++ api/app/services/organization/project_versions/build_pdf_job_arguments.rb
@@ -0,0 +1,30 @@
+module Organization
+  module ProjectVersions
+    class BuildPdfJobArguments
+      class << self
+        def call(version)
+          version_identifier = Organization::ProjectVersions::BuildVersionNumber.call(version).data
+          unless version_identifier
+            raise Error::UnprocessableEntityError, "Failed to compute version number"
+          end
+
+          ServiceResult.success({
+            "url" => find_print_url(version),
+            "class_name" => version.class.name,
+            "id" => version.id,
+            "file_name" => "#{version_identifier}.pdf"
+          })
+        rescue StandardError => e
+          ServiceResult.failure(e)
+        end
+
+        private
+
+        def find_print_url(version)
+          host = Rails.configuration.headless_browser.fetch(:app_host)
+          Rails.application.routes.url_helpers.quote_prints_url(version.project.id, version.id, { host: host })
+        end
+      end
+    end
+  end
+end
