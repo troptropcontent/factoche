02c9f9b58c6717d6d6e94318254dc7abd1af2f8c
Tom <tomecrepont@gmail.com>
2025-03-28 15:38:06 +0000

    create the CreditNoteCompact Type

--- frontend/src/components/pages/companies/invoices/shared/types.ts
+++ frontend/src/components/pages/companies/invoices/shared/types.ts
@@ -7,4 +7,7 @@ interface Line {
 type InvoiceCompact =
   paths["/api/v1/organization/companies/{company_id}/invoices"]["get"]["responses"]["200"]["content"]["application/json"]["results"][number];
 
-export type { Line, InvoiceCompact };
+type CreditNoteCompact =
+  paths["/api/v1/organization/companies/{company_id}/credit_notes"]["get"]["responses"]["200"]["content"]["application/json"]["results"][number];
+
+export type { Line, InvoiceCompact, CreditNoteCompact };
