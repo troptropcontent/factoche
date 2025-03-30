3140f712a99a40c94ada97b1c0be3d2d8a09c51a
Tom <tomecrepont@gmail.com>
2025-03-30 16:28:18 +0000

    release hte company update endpoint

--- api/app/controllers/api/v1/organization/companies_controller.rb
+++ api/app/controllers/api/v1/organization/companies_controller.rb
@@ -12,4 +12,40 @@ class Api::V1::Organization::CompaniesController < Api::V1::ApiV1Controller
 
     render json: Organization::Companies::ShowDto.new({ result: company }).to_json
   end
+
+  # PUT /api/v1/organization/companies/:id
+  def update
+    company = policy_scope(Organization::Company).find(params[:id])
+
+    result = ::Organization::Companies::Update.call(company.id, update_company_params.to_h)
+
+    raise Error::UnprocessableEntityError, result.error if result.failure?
+
+    render json: Organization::Companies::ShowDto.new({ result: result.data }).to_json
+  end
+
+  private
+
+  def update_company_params
+    params.require(:company).permit(
+      :name,
+      :registration_number,
+      :email,
+      :phone,
+      :address_city,
+      :address_street,
+      :address_zipcode,
+      :legal_form,
+      :rcs_city,
+      :rcs_number,
+      :vat_number,
+      :capital_amount,
+      configs: %i[
+        general_terms_and_condition
+        default_vat_rate
+        payment_term_days
+        payment_term_accepted_methods
+      ]
+    )
+  end
 end
--- api/spec/controllers/api/v1/organization/companies_controller_spec.rb
+++ api/spec/controllers/api/v1/organization/companies_controller_spec.rb
@@ -89,5 +89,119 @@ RSpec.describe Api::V1::Organization::CompaniesController, type: :request do
         run_test!
       end
     end
+
+    put 'Updates a company' do
+      tags 'Companies'
+      security [ bearerAuth: [] ]
+      produces 'application/json'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :body, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          registration_number: { type: :string },
+          email: { type: :string },
+          phone: { type: :string },
+          address_city: { type: :string },
+          address_street: { type: :string },
+          address_zipcode: { type: :string },
+          legal_form: { type: :string, enum: Organization::Company.legal_forms.values },
+          rcs_city: { type: :string },
+          rcs_number: { type: :string },
+          vat_number: { type: :string },
+          capital_amount: { type: :number },
+          configs: {
+            type: :object,
+            properties: {
+              general_terms_and_conditions: { type: :string },
+              default_vat_rate: { type: :number },
+              payment_term_days: { type: :integer },
+              payment_term_accepted_methods: { type: :array, items: { type: :string, enum: Organization::CompanyConfig::ALLOWED_PAYMENT_METHODS } }
+            }
+          }
+        }
+      }
+
+      let(:user) { FactoryBot.create(:user) }
+      let(:company) { FactoryBot.create(:company, :with_config) }
+      let!(:member) { FactoryBot.create(:member, user:, company:) }
+      let(:id) { company.id }
+
+      response '200', 'company updated successfully' do
+        schema ::Organization::Companies::ShowDto.to_schema
+
+        let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(user.id)}" }
+        let(:body) do
+          {
+              name: 'Updated Company Name',
+              registration_number: 'NEW123',
+              email: 'updated@example.com',
+              phone: "+33611111111",
+              address_city: 'New City',
+              address_street: 'New Street',
+              address_zipcode: '12345',
+              configs: {
+                default_vat_rate: "0.1"
+              }
+            }
+        end
+
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data.dig("result", "name")).to eq('Updated Company Name')
+          expect(data.dig("result", "registration_number")).to eq('NEW123')
+          expect(data.dig("result", "email")).to eq('updated@example.com')
+          expect(data.dig("result", "phone")).to eq("+33611111111")
+          expect(data.dig("result", "address_city")).to eq('New City')
+          expect(data.dig("result", "address_street")).to eq('New Street')
+          expect(data.dig("result", "address_zipcode")).to eq('12345')
+          expect(data.dig("result", "config", "default_vat_rate")).to eq("0.1")
+        end
+      end
+
+      response '422', 'invalid request' do
+        schema ApiError.schema
+
+        let(:user) { FactoryBot.create(:user) }
+        let(:company) { FactoryBot.create(:company, :with_config) }
+        let!(:member) { FactoryBot.create(:member, user:, company:) }
+        let(:id) { company.id }
+        let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(user.id)}" }
+        let(:body) do
+          {
+            company: {
+              name: '', # invalid empty name
+              email: 'not_an_email' # invalid email format
+            }
+          }
+        end
+
+        run_test!
+      end
+
+      response '404', 'company not found' do
+        context "when the company does not exists" do
+          let(:user) { FactoryBot.create(:user) }
+          let(:id) { 999999 }
+          let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(user.id)}" }
+          let(:body) { { company: { name: 'New Name' } } }
+
+          schema ApiError.schema
+
+          run_test! "it returns a 404"
+        end
+
+        context "when the use is not a member of the company" do
+          let(:another_user) { FactoryBot.create(:user) }
+          let(:Authorization) { "Bearer #{JwtAuth.generate_access_token(another_user.id)}" }
+          let(:body) { { company: { name: 'New Name' } } }
+
+          schema ApiError.schema
+
+          run_test! "it returns a 404"
+        end
+      end
+    end
   end
 end
