974b869f7d32202de6c024527cdc6115349e6be9
Tom <tomecrepont@gmail.com>
2025-04-24 08:16:48 +0000

    fix FindNextAvailableNumber to now consider the last number assigned

--- api/app/services/accounting/financial_transactions/find_next_available_number.rb
+++ api/app/services/accounting/financial_transactions/find_next_available_number.rb
@@ -1,23 +1,25 @@
 module Accounting
   module FinancialTransactions
     class FindNextAvailableNumber
-      class << self
-        def call(company_id:, prefix:, issue_date: Time.current)
-          raise ArgumentError, "Company ID is required" if company_id.blank?
-          raise ArgumentError, "Prefix is required" if prefix.blank?
+      include ApplicationService
 
-          transaction_count = FinancialTransaction
-            .where(company_id: company_id)
-            .where(issue_date: issue_date.beginning_of_year..issue_date.end_of_year)
-            .where("number LIKE ?", "#{prefix}%")
-            .count
+      def call(company_id:, prefix:, issue_date: Time.current)
+        raise ArgumentError, "Company ID is required" if company_id.blank?
+        raise ArgumentError, "Prefix is required" if prefix.blank?
 
-          new_number = [ prefix, issue_date.year, (transaction_count + 1).to_s.rjust(6, "0") ].join("-")
+        base_prefix = "#{prefix}-#{issue_date.year}"
 
-          ServiceResult.success(new_number)
-        rescue StandardError => e
-          ServiceResult.failure("Failed to find next available number: #{e.message}")
-        end
+        last_financial_transaction = FinancialTransaction
+          .where(company_id: company_id)
+          .where(issue_date: issue_date.beginning_of_year..issue_date.end_of_year)
+          .where("number LIKE ?", "#{base_prefix}%")
+          .order(number: :desc).first
+
+        last_financial_transaction_sequencial_identifier = last_financial_transaction ? last_financial_transaction.number.delete_prefix("#{base_prefix}-").to_i : 0
+
+        new_number = [ prefix, issue_date.year, (last_financial_transaction_sequencial_identifier + 1).to_s.rjust(6, "0") ].join("-")
+
+        new_number
       end
     end
   end
