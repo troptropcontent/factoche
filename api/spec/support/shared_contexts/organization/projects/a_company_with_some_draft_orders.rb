0def28e9f30ccc209200097edbfc629c614b39a0
Tom <tomecrepont@gmail.com>
2025-05-07 08:23:46 +0000

    fix the shared example to work with less than three projects

--- api/spec/support/shared_contexts/organization/projects/a_company_with_some_draft_orders.rb
+++ api/spec/support/shared_contexts/organization/projects/a_company_with_some_draft_orders.rb
@@ -5,9 +5,9 @@ RSpec.shared_context 'a company with some draft orders' do |number_of_draft_orde
   include_context "a company with some quotes", number_of_quotes: number_of_draft_orders
   ordinals = [ "first", "second", "third" ]
 
-  ordinals.each do |ordinal|
-    let!("#{ordinal}_draft_order") { Organization::Quotes::ConvertToDraftOrder.call(send("#{ordinal}_quote").id).data }
-    let("#{ordinal}_draft_order_version") { send("#{ordinal}_draft_order").last_version }
+  number_of_draft_orders.times do |index|
+    let!("#{ordinals[index]}_draft_order") { Organization::Quotes::ConvertToDraftOrder.call(send("#{ordinals[index]}_quote").id).data }
+    let("#{ordinals[index]}_draft_order_version") { send("#{ordinals[index]}_draft_order").last_version }
   end
 end
 # rubocop:enable RSpec/ContextWording
--- api/spec/support/shared_contexts/organization/projects/a_company_with_some_orders.rb
+++ api/spec/support/shared_contexts/organization/projects/a_company_with_some_orders.rb
@@ -5,11 +5,11 @@ require_relative "a_company_with_some_draft_orders"
 
 RSpec.shared_context 'a company with some orders' do |number_of_orders: 1|
   include_context 'a company with some draft orders', number_of_draft_orders: number_of_orders
-
   ordinals = [ "first", "second", "third" ]
-  ordinals.each do |ordinal|
-    let!("#{ordinal}_order") { Organization::DraftOrders::ConvertToOrder.call(send("#{ordinal}_draft_order").id).data }
-    let("#{ordinal}_order_version") { send("#{ordinal}_order").last_version }
+
+  number_of_orders.times do |index|
+    let!("#{ordinals[index]}_order") { Organization::DraftOrders::ConvertToOrder.call(send("#{ordinals[index]}_draft_order").id).data }
+    let("#{ordinals[index]}_order_version") { send("#{ordinals[index]}_order").last_version }
   end
 end
 # rubocop:enable RSpec/LetSetup
