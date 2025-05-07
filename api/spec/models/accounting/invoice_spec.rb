377d3b2fba84e70e9788086a3ed81f3c134f7f18
Tom <tomecrepont@gmail.com>
2025-05-07 14:30:54 +0000

    create the payment_status mehod

--- api/spec/models/accounting/invoice_spec.rb
+++ api/spec/models/accounting/invoice_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 require 'support/shared_contexts/organization/projects/a_company_with_an_order'
 RSpec.describe Accounting::Invoice, type: :model do
+  subject(:invoice) {
+    proforma = Organization::Proformas::Create.call(order_version.id, { invoice_amounts: [ { original_item_uuid: order_version.items.first.original_item_uuid, invoice_amount: "0.2" } ] }).data
+    Accounting::Proformas::Post.call(proforma.id).data
+  }
+
+  include_context 'a company with an order'
+
   describe "validations" do
-    subject(:invoice) {
-      proforma = Organization::Proformas::Create.call(order_version.id, { invoice_amounts: [ { original_item_uuid: order_version.items.first.original_item_uuid, invoice_amount: "0.2" } ] }).data
-      Accounting::Proformas::Post.call(proforma.id).data
-     }
-
-    include_context 'a company with an order'
-
     it { is_expected.to define_enum_for(:status).backed_by_column_of_type(:enum).with_values(posted: "posted", cancelled: "cancelled").with_default(:posted) }
 
     describe "#valid_number" do
@@ -69,6 +69,34 @@ RSpec.describe Accounting::Invoice, type: :model do
         }
 
         it { is_expected.not_to be_valid }
+      end
+    end
+  end
+
+  describe "instance method" do
+    describe "payment_status" do
+      context "when a payment is attached to the invoice" do
+        before { Accounting::Payments::Create.call(invoice.id) }
+
+        it "returns paid" do
+          expect(invoice.payment_status).to eq('paid')
+        end
+      end
+
+      context "when a payment is not attached to the invoice" do
+        context "when the due_date is past" do
+          before { invoice.detail.update(due_date: 2.days.ago) }
+
+          it "returns overdue" do
+            expect(invoice.payment_status).to eq('overdue')
+          end
+        end
+
+        context "when the due_date is not past" do
+          it "returns pending" do
+            expect(invoice.payment_status).to eq('pending')
+          end
+        end
       end
     end
   end
