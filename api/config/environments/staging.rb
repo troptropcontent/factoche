46b4e966a8faf6b9530adb0ccbba58472a76d1ae
Tom <tomecrepont@gmail.com>
2025-06-13 12:43:59 +0000

    setup host

--- api/config/environments/staging.rb
+++ api/config/environments/staging.rb
@@ -2,4 +2,7 @@ require_relative "production"
 
 Rails.application.configure do
   config.action_cable.allowed_request_origins = [ "https://app.staging.fabati.fr" ]
+  config.hosts = [
+    "api.staging.fabati.fr"
+  ]
 end
