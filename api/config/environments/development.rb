bf2a4728c0a169c834c703ba83a0ea03db3803ba
Tom <tomecrepont@gmail.com>
2025-06-18 13:15:36 +0000

    update accepted host

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "html_pdf:8081"
+  config.hosts << "app:8080"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
