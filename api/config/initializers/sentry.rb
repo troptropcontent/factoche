de13b7a63ce0a520fe3eac0f8675b81496d60278
Tom <tomecrepont@gmail.com>
2025-06-18 13:58:07 +0000

    remove dsn from git

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
-  config.dsn = "https://fc46ff12d12347918fd404edfb6365cd@o4509519980331008.ingest.de.sentry.io/4509519981576272"
+  config.dsn = ENV.fetch("SENTRY_DSN")
+  config.environment = ENV.fetch("RAILS_ENV")
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Add data like request headers and IP for users,
