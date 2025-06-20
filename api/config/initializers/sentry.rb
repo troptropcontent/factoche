8a6b987e8c4e6b7d9d4ec855df454859fe54d154
Tom <tomecrepont@gmail.com>
2025-06-20 08:19:15 +0000

    setup sentry

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV.fetch("SENTRY_DSN")
   config.environment = ENV.fetch("RAILS_ENV")
-  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger, :sentry_logger ]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
