5c06619716bdd832e46137fa547cd2b805a98bb6
Tom <tomecrepont@gmail.com>
2025-06-27 13:18:50 +0000

    move locales from public to src as advised by ite

--- frontend/src/config/i18n.js
+++ frontend/src/config/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationFR from '../../public/locales/fr/translation.json';
+import translationFR from '../locales/fr/translation.json';
 
 i18n
     .use(initReactI18next)
