91f8ad628c014ebe233d137a63dd11d5af62a9e7
Tom <tomecrepont@gmail.com>
2025-09-29 15:35:15 +0000

    create the service

---
+++ api/app/services/organization/invoices/find_next_snapshot_number.rb
@@ -0,0 +1,15 @@
+module Organization
+  module Invoices
+    class FindNextSnapshotNumber
+      include ApplicationService
+
+      def call(order_id)
+        order = Organization::Order.find(order_id)
+        order_version_ids = order.versions.pluck(:id)
+        already_recorded_invoice_count = Accounting::Invoice.where(holder_id: order_version_ids).count
+
+        already_recorded_invoice_count + 1
+      end
+    end
+  end
+end
