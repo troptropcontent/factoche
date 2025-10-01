96b9d762f4c4912869787f0c3a287226408cfb49
Tom <tomecrepont@gmail.com>
2025-10-01 15:10:28 +0000

    create some schema that we can re use

---
+++ api/app/contracts/accounting/project_version_schema.rb
@@ -0,0 +1,23 @@
+module Accounting
+  ProjectVersionSchema = Dry::Schema.Params do
+    required(:id).filled(:integer)
+    required(:number).filled(:integer)
+    required(:created_at).filled(:time)
+    required(:retention_guarantee_rate).filled(:decimal)
+    required(:items).array(:hash) do
+      required(:original_item_uuid).filled(:string)
+      required(:group_id).maybe(:integer)
+      required(:name).filled(:string)
+      required(:description).maybe(:string)
+      required(:quantity).filled(:integer)
+      required(:unit).filled(:string)
+      required(:unit_price_amount).filled(:decimal)
+      required(:tax_rate).filled(:decimal)
+    end
+    required(:item_groups).array(:hash) do
+      required(:id).filled(:integer)
+      required(:name).filled(:string)
+      required(:description).maybe(:string)
+    end
+  end
+end
