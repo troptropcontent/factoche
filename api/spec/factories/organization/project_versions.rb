7a47bf464cf09b260931941e85d298222f0a800d
Tom <tomecrepont@gmail.com>
2025-10-14 13:03:17 +0000

    display the general terms and condition inside the project pdf

--- api/spec/factories/organization/project_versions.rb
+++ api/spec/factories/organization/project_versions.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     retention_guarantee_rate { 0.05 }
     total_excl_tax_amount { 0.0 }
     number { 1 }
+    general_terms_and_conditions { '<h1>CONDITIONS GÉNÉRALES DE VENTE ET DE PRESTATION</h1>' }
   end
 end
